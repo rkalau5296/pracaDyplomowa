fa472323736af0466fab7ee16a91e04e5073b698
rkalau <rafal.kalata.itservice@gmail.com>
2021-03-30 23:50:14 +0200

    modify Invoice class - add new buyer details

--- src/main/java/creatorInvoice/model/invoice/buyer_name/Invoice.java
+++ src/main/java/creatorInvoice/model/invoice/buyer_name/Invoice.java
@@ -12,4 +12,11 @@ import lombok.NoArgsConstructor;
 public class Invoice {
 
     private String buyer_name;
+    private String buyer_tax_no;
+    private String buyer_post_code;
+    private String buyer_city;
+    private String buyer_street;
+    private String buyer_first_name;
+    private String buyer_country;
+
 }
